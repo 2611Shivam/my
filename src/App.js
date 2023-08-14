e1d318c1b991328becd8cb6bd3484bf9a513ea40
Shivam <kshivam7992@gmail.com>
2023-08-14 21:03:51 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Experience from './pages/Experience';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ProjectDisplay from "./pages/ProjectDisplay";
 // import ResponsiveAppBar from './components/ResponsiveAppBar';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/projects' element={<Projects/>} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path='/experience' element={<Experience/>} />
         </Routes>
         <Footer></Footer>
