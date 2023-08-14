af5ea93703a1fd47858acd3a78936c8e9f863223
Shivam <kshivam7992@gmail.com>
2023-08-15 00:13:43 +0530

    changed with my projects

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,39 +1,39 @@
-import Proj1 from "../assets/proj1.jpg";
-import Proj2 from "../assets/proj2.jpg";
-import Proj3 from "../assets/proj3.webp";
-import Proj4 from "../assets/proj4.webp";
-import Proj5 from "../assets/proj5.jpg";
-import Proj6 from "../assets/proj6.png";
+import Proj1 from "../assets/project1.png";
+import Proj2 from "../assets/project2.png";
+import Proj3 from "../assets/project3.png";
+import Proj4 from "../assets/project4.png";
+import Proj5 from "../assets/project5.png";
+import Proj6 from "../assets/project6.png";
 
 export const ProjectList = [
   {
-    name: "Dijkstra Algorithm Visualizer",
+    name: "Bank Money Transfer",
     image: Proj1,
     skills: "JavaScript,HTML,CSS",
   },
   {
-    name: "Ecommerce Website",
+    name: "Guess my number Game",
     image: Proj2,
-    skills: "React,Node.js,MongoDB",
-  },
-  {
-    name: "Spotify Clone",
-    image: Proj3,
-    skills: "React,Node.js,MongoDB,SpotifyAPI",
-  },
-  {
-    name: "Social Media Website",
-    image: Proj4,
-    skills: "React,Node.js,MySQL,GraphQL",
-  },
-  {
-    name: "Dashboard Visualizer",
-    image: Proj5,
     skills: "JavaScript,HTML,CSS",
   },
   {
-    name: "Mobile Game",
+    name: "Dice Dame",
+    image: Proj3,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Admin Dashboard",
+    image: Proj4,
+    skills: "React JS,JavaScript,HTML,CSS",
+  },
+  {
+    name: "Weather Application",
+    image: Proj5,
+    skills: "React JS,JavaScript,HTML,CSS",
+  },
+  {
+    name: "Portfolio Website",
     image: Proj6,
-    skills: "React Native,JavaScript,HTML,CSS",
+    skills: "React JS,JavaScript,HTML,CSS",
   },
 ];
